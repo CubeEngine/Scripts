0f93ce9d4d7b3078cb888d13b9451f3b80e43c49
Phillip Schichtel <quick_wango@code-infection.de>
2013-11-21 21:43:24 +0100

    updated the module generation script for the new archetype version and maven central

--- generate_module.py
+++ generate_module.py
@@ -5,75 +5,73 @@ import re
 import os
 
 def which(program):
-    for path in os.environ.get('PATH', '').split(os.pathsep):
-        if os.path.exists(os.path.join(path, program)) and \
-           not os.path.isdir(os.path.join(path, program)):
-            return os.path.join(path, program)
-    return None
+	for path in os.environ.get('PATH', '').split(os.pathsep):
+		if os.path.exists(os.path.join(path, program)) and not os.path.isdir(os.path.join(path, program)):
+			return os.path.join(path, program)
+	return None
 
 archetypeGroupId = "de.cubeisland.maven.archetypes"
 archetypeArtifactId = "archetype-cubeengine-module"
-archetypeVersion = "1.0.1"
-archetypeRepository = "http://repo.cubeisland.de/"
+archetypeVersion = "1.0.2"
+
+if os.environ['ARCHETYPE_VERSION']:
+	archetypeVersion = os.environ['ARCHETYPE_VERSION']
 
 defaultCoreVersion = "1.0.0-SNAPSHOT"
 
 moduleName = ""
 if len(argv) > 1:
-    moduleName = argv[1]
+	moduleName = argv[1]
 else:
-    moduleName = input("Enter the module name: ")
+	moduleName = input("Enter the module name: ")
 
-description = "";
+description = ""
 if len(argv) > 2:
-    description = argv[2]
+	description = argv[2]
 else:
-    description = input("Enter a short description: ")
+	description = input("Enter a short description: ")
 
 coreVersion = defaultCoreVersion
 if len(argv) > 3:
-    coreVersion = argv[3]
+	coreVersion = argv[3]
 else:
-    coreVersion = input("Enter the core version [%s]: " % defaultCoreVersion).strip()
-    if not len(coreVersion):
-        coreVersion = defaultCoreVersion
+	coreVersion = input("Enter the core version [%s]: " % defaultCoreVersion).strip()
+	if not len(coreVersion):
+		coreVersion = defaultCoreVersion
 
 groupId = "de.cubeisland.engine"
-artifactId = re.sub(r'[^a-z]', '', moduleName.lower());
+artifactId = re.sub(r'[^a-z]', '', moduleName.lower())
 
-maven = "mvn";
+maven = "mvn"
 if os.pathsep == ";":
-    maven = "mvn.bat"
+	maven = "mvn.bat"
 
 maven = which(maven)
 
 if maven == None:
-    print("I couldn't find maven in your path!")
-    exit(1)
+	print("I couldn't find maven in your path!")
+	exit(1)
 
 commandLine = [
-    maven,
-    "archetype:generate",
-    "-DarchetypeGroupId=%s" % archetypeGroupId,
-    "-DarchetypeArtifactId=%s" % archetypeArtifactId,
-    "-DarchetypeVersion=%s" % archetypeVersion,
-    "-DarchetypeRepository=%s" % archetypeRepository,
-    "-DgroupId=%s" % groupId,
-    "-DartifactId=%s" % artifactId,
-    "-Dversion=%s" % coreVersion,
-    "-Dpackage=%s.%s" % (groupId, artifactId),
-    "-Ddefault-class=%s" % (artifactId[0].upper() + artifactId[1:]),
-    "-Dname=%s" % moduleName,
-    "-Ddescription=%s" % description,
-    "-DinteractiveMode=false"
+	maven,
+	"archetype:generate",
+	"-DarchetypeGroupId=%s" % archetypeGroupId,
+	"-DarchetypeArtifactId=%s" % archetypeArtifactId,
+	"-DarchetypeVersion=%s" % archetypeVersion,
+	"-DgroupId=%s" % groupId,
+	"-DartifactId=%s" % artifactId,
+	"-Dversion=%s" % coreVersion,
+	"-Dpackage=%s.%s" % (groupId, artifactId),
+	"-Ddefault-class=%s" % (artifactId[0].upper() + artifactId[1:]),
+	"-Dname=%s" % moduleName,
+	"-Ddescription=%s" % description,
+	"-DinteractiveMode=false"
 ]
 
 result = call(commandLine)
 
 if result != 0:
-    print("It seems like maven failed to generate the module...")
-    print("Look at the output and press enter when you're done.")
-    input()
-    exit(1)
-    
-
+	print("It seems like maven failed to generate the module...")
+	print("Look at the output and press enter when you're done.")
+	input()
+	exit(1)
